94469a3a8b47987fab48103fe2782ba6c1169fad
j1803248734 <1803248734@qq.com>
2020-07-09 09:10:40 +0800

    小修改

--- src/Test.java
+++ src/Test.java
@@ -16,38 +16,38 @@ public class Test {
     public static void main(String[] args) throws ParseException {
         String uuid = UUID.randomUUID().toString().replace("-","");
         System.out.println(uuid);
-//        System.out.println(new SimpleDateFormat("yy-MM-dd hh:mm:ss").format(new SimpleDateFormat("yy-MM-dd hh:mm:ss").parse(INITIAL_DATE_STRING)));
-//        List<String> list = new ArrayList<>();
-//        list.add("one");
-//        list.add("two");
-//        Map<String, ?> keyValueMap = new HashMap<String, Object>(){
-//            {
-//                put("1",list);
-//                put("2","list");
-//                put("3",1);
-//            }
-//        };
-//        //keyset是循环key   entrySet是循环全部
-//        Object[] objects = keyValueMap.keySet().toArray();
-//        String [] keys = new String[keyValueMap.size()];
-//        for(int i = 0 ; i < objects.length  ; i++){
-//            keys[i] = (String)objects[i];
-//            System.out.println(objects[i]);
-//        }
-//
-//        int age = 10;
-//        new Student(){
-//            @Override
-//            public int getAge() {
-//                return age;
-//            }
-//        };
-//
-//        new Thread();
-//
-////        new Student( ()-> System.out.println("hhh我创建了") ).toString();
-//        new HashMap<String , String>().put("","").toCharArray();
-//        new Thread(()-> System.out.println("121")).start();
+        System.out.println(new SimpleDateFormat("yy-MM-dd hh:mm:ss").format(new SimpleDateFormat("yy-MM-dd hh:mm:ss").parse(INITIAL_DATE_STRING)));
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        Map<String, ?> keyValueMap = new HashMap<String, Object>(){
+            {
+                put("1",list);
+                put("2","list");
+                put("3",1);
+            }
+        };
+        //keyset是循环key   entrySet是循环全部
+        Object[] objects = keyValueMap.keySet().toArray();
+        String [] keys = new String[keyValueMap.size()];
+        for(int i = 0 ; i < objects.length  ; i++){
+            keys[i] = (String)objects[i];
+            System.out.println(objects[i]);
+        }
+
+        int age = 10;
+        new Student(){
+            @Override
+            public int getAge() {
+                return age;
+            }
+        };
+
+        new Thread();
+
+//        new Student( ()-> System.out.println("hhh我创建了") ).toString();
+        new HashMap<String , String>().put("","").toCharArray();
+        new Thread(()-> System.out.println("121")).start();
         
     }
 }
